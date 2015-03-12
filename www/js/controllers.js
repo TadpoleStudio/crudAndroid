fba6ac5508c43b7683dfe7342d61609b940b8672
Jerry Jiang <leeming-jiang@gmail.com>
2015-03-12 16:36:24 +0800

    commit code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,36 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function ($scope) {
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope, CustomerService) {
+        $scope.settings = {
+            enableFriends: true
+        };
+        $scope.customer = null;
+        CustomerService.showCustomer($scope);
+    })
+    .controller('ContentController', function ($scope, $ionicSideMenuDelegate) {
+        $scope.toggleLeft = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
+    })
+    .controller('CustomerController', function () {
+
+    })
+    .controller('MyCtrl', function ($scope, $ionicHistory) {
+        $scope.myGoBack = function () {
+            $ionicHistory.goBack();
+        }
+    });
